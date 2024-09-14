d94c32d6d1489e42bdec76a0ded0b13f7485b3d6
Vlad2991 <vlad29911710@gmail.com>
2024-09-14 21:13:41 +0300

    finish brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketsMap = new Map(bracketsConfig);
+
+  for (let char of str) {
+    if (bracketsMap.has(char)) {
+      if (stack.length && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(bracketsMap.get(char));
+      }
+    } else {
+      if (stack.length === 0 || stack.pop() !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
